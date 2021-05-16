e5bc8014e5757aacc11648b1ee0471154982a66c
priya-002 <priya.md0002@gmail.com>
2021-05-16 19:05:29 +0530

    UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Navbar from './navbar/Navbar';
+import Header from './components/header/Header';
+import Navbar from "./components/navbar/Navbar";
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+     <Navbar/>
+     <Header/>
+      
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import best from '../../assets/best.jpg';
+import logo from '../../assets/logo.png';
+import "./header.css"
+
+const Header = () => {
+    return (
+        <div>
+            <img className="header__logo"src={logo} alt="" />
+         <img className="header__image"src={best} alt="" />   
+        </div>
+    )
+}
+
+export default Header
