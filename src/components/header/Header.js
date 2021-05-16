a647fad76328bb407bb9088b7d4c8bfea7f63e1e
priya-002 <priya.md0002@gmail.com>
2021-05-16 21:59:15 +0530

    UI

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import best from '../../assets/best.jpg';
 import logo from '../../assets/logo.png';
+import name from '../../assets/name.png';
 import "./header.css"
 
 const Header = () => {
     return (
         <div>
+          
             <img className="header__logo"src={logo} alt="" />
+          
          <img className="header__image"src={best} alt="" />   
         </div>
     )
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import {useState} from "react"
+import ham from "../../assets/ham.png"
+import x from "../../assets/x.png"
+import "./navabar.css"
+import ReactiveButton from 'reactive-button';
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(true)
+
+    const change =()=>{
+        setOpen(!open)
+    }
     return (
-        <div>
-            
+        <div className="navbar">
+    
+        <div className ="navbar__menu" onClick={change}>
+        { open?<img className="navbar__image" src={ham} /> :<img className="navbar__image" src={x} />}
+        </div>
+        <div className={open?"navbar__list__none":"navbar__list"}>
+            <ul className="navbar__list__names">
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+               
+            </ul>
+        </div>
         </div>
     )
 }
