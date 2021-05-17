d7382c1066ddcc640b31bd40c55b57ee625e5ed8
priya-002 <priya.md0002@gmail.com>
2021-05-17 19:47:01 +0530

    calculator

--- src/components/calculator/Cal.js
+++ src/components/calculator/Cal.js
@@ -5,9 +5,10 @@ const Cal = () => {
     const [input, setInput] = useState({firstName:'',lastName:'',number:0,city:'',state:'',loanAmount:0,purpose:'',interest:0,interestInstallment:''})
     const ans =(e)=>{
         setInput({...input,[e.target.name]:e.target.value})
-       
-
-    }
+      }
+      const res=()=>{
+        console.log(input)
+       }
     return (
         <div>
         <form >
@@ -16,24 +17,44 @@ const Cal = () => {
          type="text" onChange={ans}
          required
         />
-        <label>Rate</label>
-        <input 
+        <label>Last Name</label>
+        <input name='lasttName'
+         type="text" onChange={ans}
+         required
+        />
+        <label>First Name</label>
+        <input name='firstName'
+         type="text" onChange={ans}
+         required
+        />
+        <label>Contact Number</label>
+        <input name='number'
          type="number" onChange={ans}
          required
         />
-        
-        <label>Interest</label>
-        <input 
+        <label>City</label>
+        <input name='city'
+         type="text" onChange={ans}
+         required
+        />
+        <label>State</label>
+        <input name='state'
+         type="text" onChange={ans}
+         required
+        />
+         <label>Loan Amount</label>
+        <input name='loanAmount'
          type="number" onChange={ans}
          required
         />
-        <label>Blog author:</label>
-        <select>
-          
-        
-          <option value="Robin">Robin</option>
-          <option value="Shenoy">Shenoy</option>
-        </select>
+       
+       <label>purpose</label>
+        <input name='purpose'
+         type="text" onChange={ans}
+         required
+        />
+        <button onClick={res}>Submit</button>
+      
        
       </form>
         </div>
