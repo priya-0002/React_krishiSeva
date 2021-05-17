30a76e9d558b21c7b07774fcf0bbadfc425586fb
priya-002 <priya.md0002@gmail.com>
2021-05-17 20:37:34 +0530

    calculator

--- src/components/calculator/Cal.js
+++ src/components/calculator/Cal.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {useState} from 'react'
-
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 const Cal = () => {
     const [input, setInput] = useState({firstName:'',lastName:'',number:0,city:'',state:'',loanAmount:0,purpose:'',interest:0,interestInstallment:''})
     const ans =(e)=>{
@@ -11,52 +14,63 @@ const Cal = () => {
        }
     return (
         <div>
-        <form >
-        <label>First Name</label>
-        <input name='firstName'
-         type="text" onChange={ans}
-         required
-        />
-        <label>Last Name</label>
-        <input name='lasttName'
-         type="text" onChange={ans}
-         required
-        />
-        <label>First Name</label>
-        <input name='firstName'
-         type="text" onChange={ans}
-         required
-        />
-        <label>Contact Number</label>
-        <input name='number'
-         type="number" onChange={ans}
-         required
-        />
-        <label>City</label>
-        <input name='city'
-         type="text" onChange={ans}
-         required
-        />
-        <label>State</label>
-        <input name='state'
-         type="text" onChange={ans}
-         required
-        />
-         <label>Loan Amount</label>
-        <input name='loanAmount'
-         type="number" onChange={ans}
-         required
-        />
-       
-       <label>purpose</label>
-        <input name='purpose'
-         type="text" onChange={ans}
-         required
-        />
-        <button onClick={res}>Submit</button>
-      
-       
-      </form>
+       <Card className="text-center">
+  <Card.Header>Featured</Card.Header>
+  <Card.Body>
+  <Form className="p">
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group controlId="formGridAddress1">
+    <Form.Label>Address</Form.Label>
+    <Form.Control placeholder="1234 Main St" />
+  </Form.Group>
+
+  <Form.Group controlId="formGridAddress2">
+    <Form.Label>Address 2</Form.Label>
+    <Form.Control placeholder="Apartment, studio, or floor" />
+  </Form.Group>
+
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>City</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>State</Form.Label>
+      <Form.Control as="select" defaultValue="Choose...">
+        <option>Choose...</option>
+        <option>...</option>
+      </Form.Control>
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridZip">
+      <Form.Label>Zip</Form.Label>
+      <Form.Control />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group id="formGridCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+  </Card.Body>
+  <Card.Footer className="text-muted">2 days ago</Card.Footer>
+</Card>
         </div>
     )
 }
